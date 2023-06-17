4fd0e3c73aa4ab81bf01f186a9eca15926590781
El-tiene <85236783+El-tiene@users.noreply.github.com>
2023-06-17 19:43:47 +0200

    temp

--- App.js
+++ App.js
@@ -1,55 +1,8 @@
-// App.js
-
 import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
-
-
-import Example from './components/Exercise2';
-import Exo3 from './components/Exo3';
+import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () => {
-  return (
-    <SafeAreaView style={styles.safeArea}>
-      <Example/>
-    </SafeAreaView>
-  );
+  return <AppNavigator />;
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingVertical: 20,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 20,
-  },
-  header: {
-    backgroundColor: 'grey',
-    padding: 30,
-  },
-  headerText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#fff',
-  },
-  boxContainer: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  box: {
-    height: 50,
-    marginBottom: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  boxText: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: 'red',
-  },
-});
